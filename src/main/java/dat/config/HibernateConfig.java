216dd1093bed6d1d34eb5a70c034683e22925fda
vaangis93 <vaangis93@gmail.com>
2025-01-10 12:10:24 +0100

    changed hibernate to update instead of create-drop so it doesnt clear the DB each run

--- src/main/java/dat/config/HibernateConfig.java
+++ src/main/java/dat/config/HibernateConfig.java
@@ -78,7 +78,7 @@ public class HibernateConfig {
     private static Properties setBaseProperties(Properties props) {
         props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        props.put("hibernate.hbm2ddl.auto", "create");
+        props.put("hibernate.hbm2ddl.auto", "update");
         props.put("hibernate.current_session_context_class", "thread");
         props.put("hibernate.show_sql", "true");
         props.put("hibernate.format_sql", "true");
