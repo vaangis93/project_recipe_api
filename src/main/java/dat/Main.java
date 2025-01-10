c808f6b4ead3cd3a399c13222b388cb8f954bf26
vaangis93 <vaangis93@gmail.com>
2025-01-10 12:00:47 +0100

    changed hibernate to update instead of create-drop so it doesnt clear the DB each run

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -14,6 +14,6 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7070);
-        // bla bla bla bla bla bla
+        // bla bla bla bla bla bla bla bla
     }
 }
